452b93c600571cd87f67b9c492b6d19a301d868b
TEC037 <105821219+TEC037@users.noreply.github.com>
2026-07-30 13:21:41 -0500

    chore: start reward handler on app bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import './index.css';
 import App from './App.svelte';
 import { mount } from 'svelte';
+import { startRewardHandler } from './lib/rewardHandler';
 
 const app = mount(App, {
   target: document.getElementById('root')!,
 });
+
+// Start reward handler for global reward events
+startRewardHandler();
 
 export default app;
